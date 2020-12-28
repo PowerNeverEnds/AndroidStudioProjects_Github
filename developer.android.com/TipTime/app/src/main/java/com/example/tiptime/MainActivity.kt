2a690d77a3941b020b3bbf2df14989f58f2daa8e
PowerNeverEnds <PowerNeverEnds8@gmail.com>
2020-12-28 17:13:16 +0900

    Unit 2: Layouts - Get user input in an app - 5Calculate the tip ( 6Change the app theme부터 시작)

--- developer.android.com/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ developer.android.com/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+//            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        displayTip(tip)
+//        println(stringInTextField)
+//        println(cost)
+//        val a = R.string.tip_amount
+//        val b = R.string.amazing_service
+//        println("${a} ${b}")
+//        println(getString(R.string.tip_amount))
+//        println(getString(R.string.tip_amount, formattedTip))
+//        println(getString(R.string.tip_amount, "abc"))
+//        println(R.string.tip_amount)
+//        println(formattedTip)
+//        println(binding.tipResult.text)
+    }
+
+    private fun displayTip(tip:Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+    }
+
 }
