72d55fbc64fd326e00ab5fe29bdddf1138c25f02
PowerNeverEnds <PowerNeverEnds8@gmail.com>
2021-03-13 19:07:37 +0900

    모바일소프트웨어 SOFT425 02주차 실습   - 1단계 문자 변경 예제 결과 화면 [P.19]

--- MobileSoftware_SOFT425/HelloAndroid/app/src/main/java/com/example/helloandroid/HelloAndroidActivity.kt
+++ MobileSoftware_SOFT425/HelloAndroid/app/src/main/java/com/example/helloandroid/HelloAndroidActivity.kt
@@ -3,9 +3,9 @@ package com.example.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HelloAndroidActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello_android)
     }
 }
