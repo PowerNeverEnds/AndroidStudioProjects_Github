41ebca68a4a1dc836e041126e841b0d0b75dbb58
PowerNeverEnds <PowerNeverEnds8@gmail.com>
2021-03-13 20:00:33 +0900

    모바일소프트웨어 SOFT425 02주차 실습   - 3단계 버튼 이벤트 처리 예제 결과 화면 [P.26]

--- MobileSoftware_SOFT425/HelloAndroid/app/src/main/java/com/example/helloandroid/HelloAndroidActivity.kt
+++ MobileSoftware_SOFT425/HelloAndroid/app/src/main/java/com/example/helloandroid/HelloAndroidActivity.kt
@@ -1,11 +1,28 @@
 package com.example.helloandroid
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.ButtonBarLayout
 
 class HelloAndroidActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello_android)
+
+ //       val Button: startBtn = findViewById(R.id.startBtn);
+
+        val startBtn = findViewById<View>(R.id.startBtn);
+
+//        val startBtn = findViewById<android.view.View>(R.id.startBtn) as Button
+        startBtn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "시작 버튼이 눌렸어요.", Toast.LENGTH_LONG).show()
+            }
+        })
+
     }
 }
