a7a0be9767c4b11b7b1684a06500f8353bb60473
PowerNeverEnds <PowerNeverEnds8@gmail.com>
2021-05-14 07:24:14 +0900

    MyWeight BottomNavigation 생성
    https://m.blog.naver.com/wkdgus1164/222024242871
    https://berkbach.com/configure-bottom-navigation-330f2c0d9832

--- SoftwareProject2_SOFT472/MyWeight/app/src/main/java/com/example/myweight/MainActivity.kt
+++ SoftwareProject2_SOFT472/MyWeight/app/src/main/java/com/example/myweight/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.myweight
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity :AppCompatActivity(){
+    private val fragmentManager: FragmentManager = supportFragmentManager
+//    private var transaction: FragmentTransaction = fragmentManager.beginTransaction()
+    private val fragmentButton: ButtonFragment = ButtonFragment()
+    private val fragmentCard: CardFragment = CardFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
     }
+
+    private fun initView(){
+        val tv:TextView = findViewById<TextView>(R.id.textView)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            val transaction = fragmentManager.beginTransaction()
+             when(item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    println("page_1")
+                    Toast.makeText(this.applicationContext, "Al", Toast.LENGTH_SHORT).show()
+//                    startActivity(Intent(this, MainActivity::class.java))
+                    transaction.replace(R.id.frameLayout, fragmentButton).commitAllowingStateLoss()
+
+                    tv.setText("Medium!")
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    println("page_2")
+//                    startActivity(Intent(this, MainActivity::class.java))
+                    transaction.replace(R.id.frameLayout, fragmentCard).commitAllowingStateLoss()
+                    tv.setText("Wasd!")
+
+                    true
+                }
+                 R.id.page_3 -> {
+                    transaction.replace(R.id.frameLayout, fragmentButton).commitAllowingStateLoss()
+                     tv.setText("Android!")
+                     true
+                 }
+                else -> {
+
+//                    startActivity(Intent(this, MainActivity::class.java))
+                    false
+                }
+            }
+//            true
+        }
+    }
+
 }
